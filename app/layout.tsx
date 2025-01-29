7d08bdcf7fd43d365d2a6b2ebed61f86bd694227
IFTE-13 <ifte.phoenix@gmail.com>
2025-01-29 21:05:13 +0600

    feat (layout): new mode switcher component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-// app/layout.tsx
+import "@/styles/globals.css";
 import { Inter } from "next/font/google"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/shared/Navbar"
+import Footer from "@/components/shared/Footer";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -12,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
@@ -20,9 +21,8 @@ export default function RootLayout({
           enableSystem
         >
           <Navbar />
-          <main className="container mx-auto px-4 py-8">
-            {children}
-          </main>
+          {children}
+          <Footer />
           <Toaster />
         </ThemeProvider>
       </body>
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -6,47 +6,51 @@ import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/co
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import { navLinks, siteName } from '@/lib/constants';
 import { Menu } from "lucide-react"
+import { ModeToggle } from '../ToggleMode';
 
 const Navbar = () => {
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all dark:bg-black">
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-gray-200 bg-white/75 backdrop-blur-lg transition-all dark:bg-black">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between">
           <Link href="/" className="flex z-40 font-semibold">
             <span>{siteName}</span>
           </Link>
 
-          <div className="hidden items-center space-x-4 sm:flex">
-            {navLinks.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className={buttonVariants({
-                  variant: link.variant,
-                  size: 'sm',
-                })}
-              >
-                {link.label}
-              </Link>
-            ))}
-          </div>
+          
+          <div className='flex flex-row space-x-4'>
+            <div className="hidden items-center space-x-4 sm:flex">
+              {navLinks.map((link) => (
+                <Link
+                  key={link.href}
+                  href={link.href}
+                  className={buttonVariants({
+                    variant: link.variant,
+                    size: 'sm',
+                  })}
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </div>
+            <div className="flex justify-start">
+              <ModeToggle />
+            </div>
 
           <div className="sm:hidden">
             <Sheet>
               <SheetTrigger asChild>
                 <Button 
-                  variant="ghost" 
-                  size="sm"
-                  className="h-8 w-8 px-0"
+                  variant="ghost"
                 >
                   <Menu className="h-5 w-5" />
                   <span className="sr-only">Toggle menu</span>
                 </Button>
               </SheetTrigger>
               <SheetContent side="right" className="w-[300px] sm:w-[400px]">
-              <SheetHeader>
-                <SheetTitle>Bloggy</SheetTitle>
-              </SheetHeader>
+                <SheetHeader>
+                  <SheetTitle>Bloggy</SheetTitle>
+                </SheetHeader>
                 <div className="flex flex-col space-y-4 mt-6">
                   {navLinks.map((link) => (
                     <Link
@@ -64,6 +68,8 @@ const Navbar = () => {
                 </div>
               </SheetContent>
             </Sheet>
+          </div>
+          
           </div>
         </div>
       </MaxWidthWrapper>
