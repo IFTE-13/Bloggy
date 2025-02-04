1929812509d87fc25abd24d6ca6823a4854d084d
IFTE-13 <ifte.phoenix@gmail.com>
2025-02-04 15:02:28 +0600

    style: navbar and footer style updated

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,24 +1,20 @@
-import { siteName } from "@/lib/constants"
-import MaxWidthWrapper from "../MaxWidthWrapper"
 import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div className="bg-black border-t">
-        <MaxWidthWrapper>
-            <div className="flex flex-row justify-between items-center h-14">
-              <p className="text-muted text-xs text-center">© Rights Reserved by {" "}
-                <Link href={"/"} className="hover:text-white/75 transition-all duration-300 underline">
-                  {siteName}
-                </Link>
-              </p>
-              <p className="text-muted text-xs text-center">Made by {" "}
-                <Link href={"https://github.com/cold-labs"} className="hover:text-white/75 transition-all duration-300 underline" target="_blank">
-                  ColdLabs
-                </Link>
-              </p>
-            </div>
-        </MaxWidthWrapper>
+    <div className="bg-black border-t px-4">
+        <div className="mx-auto max-w-4xl flex flex-row justify-between items-center h-14">
+          <p className="text-muted text-xs text-center">© Rights Reserved by {" "}
+            <Link href={"/"} className="hover:text-white/75 transition-all duration-300 underline">
+              Bloggy
+            </Link>
+          </p>
+          <p className="text-muted text-xs text-center">Made by {" "}
+            <Link href={"https://github.com/cold-labs"} className="hover:text-white/75 transition-all duration-300 underline" target="_blank">
+              ColdLabs
+            </Link>
+          </p>
+        </div>
     </div>
   )
 }
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,78 +1,44 @@
 "use client"
 import React from 'react';
 import Link from 'next/link';
-import { Button, buttonVariants } from '@/components/ui/button';
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
-import MaxWidthWrapper from '@/components/MaxWidthWrapper';
-import { navLinks, siteName } from '@/lib/constants';
-import { Menu } from "lucide-react"
-import { ModeToggle } from '../ToggleMode';
+import { buttonVariants } from '@/components/ui/button';
+import { ToggleMode } from '../ToggleMode';
+
+export const navLinks = [
+  { href: '/', label: 'Home' }
+];
 
 const Navbar = () => {
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-gray-200 bg-white/75 backdrop-blur-lg transition-all dark:bg-black">
-      <MaxWidthWrapper>
-        <div className="flex h-14 items-center justify-between">
-          <Link href="/" className="flex z-40 font-semibold">
-            <span>{siteName}</span>
-          </Link>
+        <div className='max-w-4xl mx-auto px-4'>
+          <div className="flex h-14 items-center justify-between">
+            <Link href="/" className="flex z-40 font-semibold">
+              <span>Bloggy</span>
+            </Link>
 
-          
-          <div className='flex flex-row space-x-4'>
-            <div className="hidden items-center space-x-4 sm:flex">
-              {navLinks.map((link) => (
-                <Link
-                  key={link.href}
-                  href={link.href}
-                  className={buttonVariants({
-                    variant: link.variant,
-                    size: 'sm',
-                  })}
-                >
-                  {link.label}
-                </Link>
-              ))}
+            
+            <div className='flex flex-row space-x-4'>
+              <div className="flex items-center space-x-4 sm:flex">
+                {navLinks.map((link) => (
+                  <Link
+                    key={link.href}
+                    href={link.href}
+                    className={buttonVariants({
+                      size: 'sm',
+                      variant: "ghost"
+                    })}
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </div>
+              <div className="flex justify-start">
+                <ToggleMode />
+              </div>
             </div>
-            <div className="flex justify-start">
-              <ModeToggle />
-            </div>
-
-          <div className="sm:hidden">
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button 
-                  variant="ghost"
-                >
-                  <Menu className="h-5 w-5" />
-                  <span className="sr-only">Toggle menu</span>
-                </Button>
-              </SheetTrigger>
-              <SheetContent side="right" className="w-[300px] sm:w-[400px]">
-                <SheetHeader>
-                  <SheetTitle>Bloggy</SheetTitle>
-                </SheetHeader>
-                <div className="flex flex-col space-y-4 mt-6">
-                  {navLinks.map((link) => (
-                    <Link
-                      key={link.href}
-                      href={link.href}
-                      className={buttonVariants({
-                        variant: link.variant,
-                        size: 'sm',
-                        className: 'w-full',
-                      })}
-                    >
-                      {link.label}
-                    </Link>
-                  ))}
-                </div>
-              </SheetContent>
-            </Sheet>
-          </div>
-          
           </div>
         </div>
-      </MaxWidthWrapper>
     </nav>
   );
 };
